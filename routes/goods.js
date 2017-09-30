35c6788e2d73199dd0a1ed609f6bc9324cc92ad6
Lindsay Chapin <lnchapin@gmail.com>
2017-09-30 15:11:47 -0600

    get routes

--- routes/goods.js
+++ routes/goods.js
@@ -6,4 +6,51 @@ router.get("/", function (req, res){
   res.json(database.goods)
 })
 
+router.get("/categories/:categories" , function (req, res){
+  var categories = req.params.categories;
+  var data = database.goods;
+  var response = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].categories == categories) {
+      response.push(data[i])
+    }
+  }
+  res.send(response)
+})
+
+router.get("/id/:id" , function (req, res){
+  var id = req.params.id;
+  var data = database.goods;
+  var response = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].id == id) {
+      response.push(data[i])
+    }
+  }
+  res.send(response)
+})
+
+router.get("/name/:name" , function (req, res){
+  var name = req.params.name;
+  var data = database.goods;
+  var response = [];
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].name == name) {
+      response.push(data[i])
+    }
+  }
+  res.send(response)
+})
+// 
+// router.post('/:id', function(req, res) {
+//   var id = req.params.id;
+// });
+//
+// router.put('/:id', function(req, res) {
+//
+// });
+
 module.exports = router;
