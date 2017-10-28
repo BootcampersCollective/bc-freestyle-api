74122021e6c9c78c71f8cdd6dcb451da7c977662
alvaro911 <algp232@hotmail.com>
2017-10-28 15:02:08 -0600

    Created Goods model

---
+++ src/models/Goods.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const GoodsSchema = new Schema({
+  "haves": Boolean,
+  mail :String,
+  user : {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  name :String,
+  postDate: Date,
+  categories: [],
+  desc : String,
+  qty: String,
+  location:{
+    lat: String,
+    long: String,
+  },
+  img: "URL:String",
+  pickUpDate: Date,
+  expDate : Date,
+  availTime: String
+});
+
+const Goods = mongoose.model('GoodsModel', GoodsSchema);
+
+export default Goods;
